74669742ce329882df6673d52386c9e7dfd1cddb
Gor Arakelyan <gora@redkite.io>
2023-04-09 11:11:51 +0400

    add tooltip to IconButton

--- src/components/navigation/Utils.jsx
+++ src/components/navigation/Utils.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { IconButton } from '@material-ui/core';
+import { IconButton, Tooltip } from '@material-ui/core';
 import HomeIcon from '@material-ui/icons/Home';
 import SearchIcon from '@material-ui/icons/Search';
 import FastfoodIcon from '@material-ui/icons/Fastfood';
@@ -19,16 +19,18 @@ export const IconRenderer = () => (
     icons.map((item) => {
         return (
             <React.Fragment key={item.page}>
-                <IconButton
-                    aria-label="account of current user"
-                    aria-controls="primary-search-account-menu"
-                    aria-haspopup="true"
-                    color="inherit"
-                    component={Link}
-                    to={item.page}
-                >
-                    {item.icon}
-                </IconButton>
+                <Tooltip title={item.page}>
+                    <IconButton
+                        aria-label="account of current user"
+                        aria-controls="primary-search-account-menu"
+                        aria-haspopup="true"
+                        color="inherit"
+                        component={Link}
+                        to={item.page}
+                    >
+                        {item.icon}
+                    </IconButton>
+                </Tooltip>
             </React.Fragment>
         )
     })
